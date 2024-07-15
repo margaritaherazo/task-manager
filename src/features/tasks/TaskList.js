4c422996c8166c5f3e034c02046a6a8d93e5bfcb
maggi <margarita.herazo.i@gmail.com>
2024-07-15 15:04:32 +0200

    adding TaskList.js

---
+++ src/features/tasks/TaskList.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from './taskAPI';
+import { addTask, selectTasks } from './taskSlice';
+import TaskItem from '../../components/TaskItem';
+import './TaskList.css';
+
+const TaskList = () => {
+  const dispatch = useDispatch();
+  const tasks = useSelector(selectTasks);
+
+  useEffect(() => {
+    // Fetch tasks from the API and dispatch action to add tasks to Redux store
+    fetchTasks().then((tasks) => {
+      tasks.forEach(task => dispatch(addTask(task)));
+    });
+  }, [dispatch]);  // Include dispatch as dependency to avoid eslint warning
+
+  return (
+    <div className="task-list">
+      {tasks.map(task => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
