af3be0134e17e6d419ff1cad60d641661d412f6e
maggi <margarita.herazo.i@gmail.com>
2024-07-15 14:43:53 +0200

    Adding taskSlice.j and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import TaskList from './components/TaskList';
+import TaskForm from './features/tasks/TaskForm';
+import UserProfile from './features/user/UserProfile';
+import { ThemeProvider } from './context/ThemeContext';
+import './styles/global.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Switch>
+            <Route path="/" exact>
+              <TaskList />
+              <TaskForm />
+            </Route>
+            <Route path="/profile">
+              <UserProfile />
+            </Route>
+            {/* Add more routes as needed */}
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
+
+
