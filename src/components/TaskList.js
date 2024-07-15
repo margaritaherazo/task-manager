e397c286c41af74e9aa1a820d99298a5505fa762
maggi <margarita.herazo.i@gmail.com>
2024-07-15 15:17:18 +0200

    adding theme context in my TaskList component and adding index.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTask } from '../features/tasks/taskSlice';
-import TaskItem from './TaskItem';
+import { Link } from 'react-router-dom';
+import { useTheme } from '../context/ThemeContext';
+import styled from 'styled-components';
 
 const TaskList = () => {
-  const tasks = useSelector((state) => state.tasks);
+  const tasks = useSelector(state => state.tasks);
   const dispatch = useDispatch();
+  const { theme, toggleTheme } = useTheme();
 
-  const handleDelete = (id) => {
+  const handleDelete = id => {
     dispatch(removeTask(id));
   };
 
   return (
-    <div>
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} onDelete={() => handleDelete(task.id)} />
-      ))}
-    </div>
+    <TaskListWrapper theme={theme}>
+      <h1>Task List</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.title}
+            <button onClick={() => handleDelete(task.id)}>Delete</button>
+            <Link to={`/tasks/${task.id}/edit`}>Edit</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/tasks/new">Add New Task</Link>
+    </TaskListWrapper>
   );
 };
+
+const TaskListWrapper = styled.div`
+  background-color: ${({ theme }) => (theme === 'light' ? '#fff' : '#333')};
+  color: ${({ theme }) => (theme === 'light' ? '#000' : '#fff')};
+  padding: 20px;
+`;
 
 export default TaskList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/global.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from './context/ThemeContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
