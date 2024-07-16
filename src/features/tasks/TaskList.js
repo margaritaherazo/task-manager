a8f9eb495a4cbbca3496a3b0132514424d57a721
maggi <margarita.herazo.i@gmail.com>
2024-07-16 11:37:06 +0200

    update TaskSlice.js

--- src/features/tasks/TaskList.js
+++ src/features/tasks/TaskList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchTasks } from './taskAPI';
+import { fetchTasks } from './TaskAPI';
 import { addTask, selectTasks } from './taskSlice';
 import TaskItem from '../../components/TaskItem';
 import './TaskList.css';
--- src/features/tasks/TaskSlice.js
+++ src/features/tasks/TaskSlice.js
@@ -10,8 +10,22 @@ const taskSlice = createSlice({
     removeTask: (state, action) => {
       return state.filter(task => task.id !== action.payload);
     },
+    toggleTask: (state, action) => {
+      const task = state.find(task => task.id === action.payload);
+      if (task) {
+        task.completed = !task.completed;
+      }
+    },
+    updateTask: (state, action) => {
+      const { id, title } = action.payload;
+      const task = state.find(task => task.id === id);
+      if (task) {
+        task.title = title;
+      }
+    }
   },
 });
 
-export const { addTask, removeTask } = taskSlice.actions;
+export const { addTask, removeTask, toggleTask, updateTask } = taskSlice.actions;
 export default taskSlice.reducer;
+
