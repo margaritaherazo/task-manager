0e0c189e270fae7e4465364c9b6d16a4a9a365ae
maggi <margarita.herazo.i@gmail.com>
2024-07-16 11:30:35 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
+import './styles/global.css';
+import './styles/theme.css';
 
 ReactDOM.render(
   <Provider store={store}>
