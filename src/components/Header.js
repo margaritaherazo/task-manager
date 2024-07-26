80bffb76cca40d5af1b3895ccaadcbfad0df7309
maggi <margarita.herazo.i@gmail.com>
2024-07-26 12:59:11 +0200

    adding user name in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const user = useSelector((state) => state.user);
+
   return (
     <header>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          {/* Add other links as needed */}
         </ul>
       </nav>
+      {user.name && <div className="user-info">{user.name}</div>}
     </header>
   );
 };
