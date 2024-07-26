7d4285a7d286d6a6f77ab84e8b3cf4daab25144e
maggi <margarita.herazo.i@gmail.com>
2024-07-26 12:07:44 +0200

    updating userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,14 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchUserProfile, updateUserProfile } from './userAPI';
+
+const initialState = {
+  profile: null,
+  status: 'idle',
+};
+
+export const getUserProfile = createAsyncThunk('user/fetchUserProfile', async () => {
+  const response = await fetchUserProfile();
+  return response;
+});
+
+export const saveUserProfile = createAsyncThunk('user/updateUserProfile', async (profile) => {
+  const response = await updateUserProfile(profile);
+  return response;
+});
 
 const userSlice = createSlice({
   name: 'user',
-  initialState: { name: 'John Doe', email: 'john.doe@example.com' },
-  reducers: {
-    updateUser: (state, action) => {
-      return { ...state, ...action.payload };
-    },
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUserProfile.fulfilled, (state, action) => {
+        state.profile = action.payload;
+      })
+      .addCase(saveUserProfile.fulfilled, (state, action) => {
+        state.profile = action.payload;
+      });
   },
 });
 
-export const { updateUser } = userSlice.actions;
 export default userSlice.reducer;
