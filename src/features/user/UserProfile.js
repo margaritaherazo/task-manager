5f63adcad1958133029d48990132d0bf35229a7d
maggi <margarita.herazo.i@gmail.com>
2024-07-26 12:11:29 +0200

    updating UserProfile.js

--- src/features/user/UserProfile.js
+++ src/features/user/UserProfile.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserProfile, saveUserProfile } from './userSlice';
 
 const UserProfile = () => {
-  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    dispatch(getUserProfile());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (profile) {
+      setName(profile.name);
+      setEmail(profile.email);
+    }
+  }, [profile]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(saveUserProfile({ name, email }));
+  };
 
   return (
     <div>
-      <h2>{user.name}</h2>
-      <p>Email: {user.email}</p>
+      <h1>User Profile</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
 
 export default UserProfile;
+
