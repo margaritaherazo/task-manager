705fc3df357f950147e2b967a8f0f6c64393d1f3
maggi <margarita.herazo.i@gmail.com>
2024-07-29 11:00:55 +0200

    adding title, logo and classes to my header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,32 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useTheme } from '../context/ThemeContext';
+import logo from '../imgs/logo.png';
 
 const Header = () => {
   const user = useSelector((state) => state.user);
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <div className="header-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>Task Manager</h1>
+      </div>
+      <nav className="header-nav">
         <ul className="nav-list">
-          <li>
-            <Link className="nav-link"  to="/">Home</Link>
-          </li>
-          <li>
-            <Link className="nav-link"  to="/profile">Profile</Link>
-          </li>
-          {/* Add other links as needed */}
+          <li><Link className="nav-link" to="/">Home</Link></li>
+          <li><Link className="nav-link" to="/profile">Profile</Link></li>
         </ul>
       </nav>
-      {user.name && <div className="user-info">{user.name}</div>}
-      <button onClick={toggleTheme}>
-        Switch to {theme === 'light' ? 'dark' : 'light'} theme
-      </button>
+      <div className="header-right">
+        {user.name && <div className="user-info">{user.name}</div>}
+        <button onClick={toggleTheme} className="theme-switch-button">
+          Switch to {theme === 'light' ? 'dark' : 'light'} theme
+        </button>
+      </div>
     </header>
   );
 };
 
 export default Header;
-
